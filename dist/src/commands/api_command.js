d9b3ff82c91b989ffe9d0f60af622618810f5963
ViKing-Coder-jpg <vinaying342@gmail.com>
2026-03-05 11:22:12 +0530

    done

--- dist/src/commands/api_command.js
+++ dist/src/commands/api_command.js
@@ -5,21 +5,27 @@ class APICommand {
     }
     register() {
         this.program
-            .command('where is the city of <city>').action(async (city) => this.findCity(city));
+            .command('where is the city of <city>').action(async (city) => this.findCity(city))
+            .command('what type of pokemon is <pokemon>').action(async (pokemon) => this.typeTell(pokemon));
     }
     async findCity(city) {
         try {
-            const response = await axios.get(`https://geocode.maps.co/search?q=${city}`);
-            if (response.data.length > 0) {
-                const location = response.data[0];
-                console.log(`City: ${city} is located at latitude ${location.lat}, longitude ${location.lon}`);
-            }
-            else {
-                console.log(`City ${city} not found`);
-            }
+            const response = await axios.get(`https://geocode.maps.co/search?q=${city}&api_key=69a906deb93f4970535107xrz6b58be`);
+            const country = response[0].address.country;
+            console.log(`The city of ${city} is in ${country}`);
         }
         catch (error) {
             console.error('Error fetching city location:', error.message);
+        }
+    }
+    async typeTell(pokemon) {
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            const type = response.types.reduce((full, curr) => full + '-' + curr.type.name, '');
+            console.log(`The pokemon ${pokemon} is of ${type} type`);
+        }
+        catch (error) {
+            console.error('Error fetching pokemon:', error.message);
         }
     }
 }
--- src/commands/api_command.ts
+++ src/commands/api_command.ts
@@ -8,6 +8,8 @@ class APICommand {
     register() {
         this.program
             .command('where is the city of <city>').action(async (city: string) => this.findCity(city))
+            .command('what type of pokemon is <pokemon>').action(async (pokemon: string) => this.typeTell(pokemon))
+            
     }
     async findCity(city: string) {
         try {
@@ -16,6 +18,15 @@ class APICommand {
             console.log(`The city of ${city} is in ${country}`)
         } catch (error: any) {
             console.error('Error fetching city location:', error.message);
+        }
+    }
+    async typeTell(pokemon: string) {
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            const type=response.types.reduce((full,curr)=>full+'-'+curr.type.name,'')
+            console.log(`The pokemon ${pokemon} is of ${type} type`)
+        } catch (error: any) {
+            console.error('Error fetching pokemon:', error.message);
         }
     }
 }
